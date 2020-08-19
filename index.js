c32e0a70b7b4dc1a242d4883fcaf64b1309629f3
Mary <von47us@yahoo.com>
2020-08-19 23:05:06 +0100

    My new site upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+ document.querySelector('input').setAttribute("value", window.innerWidth +'x'+window.innerHeight);
+
+// $('img.tail').remove();
+// $('.cont11').css('background-Color','black');
+
+$('img.ceevee').css('box-Shadow','0 0 20px #c1c1c1fc');
