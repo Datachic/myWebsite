08467cfb20fbe1dc41bd0e644465378db46d1c53
Mary <von47us@yahoo.com>
2020-08-22 01:56:57 +0100

    trial

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
- document.querySelector('input').setAttribute("value", window.innerWidth +'x'+window.innerHeight);
+var y = document.querySelectorAll('input');
+for (let i=0;i<2;i++){
+  y[i].value =window.innerWidth+'x'+ innerHeight;
 
-// $('img.tail').remove();
-// $('.cont11').css('background-Color','black');
-
-$('img.ceevee').css('box-Shadow','0 0 20px #c1c1c1fc');
+}
